9db7fbd016491a03745b5596f811429675393131
Jm <155852978+javiirmv@users.noreply.github.com>
2024-09-19 08:48:20 +0200

    Ejercicios clase

---
+++ src/java/Primera/Ejercicio008.java
@@ -0,0 +1,13 @@
+package Primera;
+
+public class Ejercicio008 {
+
+    public static void main(String arg[]) {
+        int x;
+        int y;
+
+        for (int i = 0; i<10; i++){
+            System.out.println("Contador: " + (i + 1));
+        }
+    }
+}
