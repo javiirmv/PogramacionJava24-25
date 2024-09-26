9e48155fab2cb7bcdcef0a460c7babe645df81f4
Jm <155852978+javiirmv@users.noreply.github.com>
2024-09-26 09:11:01 +0200

    Ejercicios clase

--- src/java/Primera/Ejercicio020.java
+++ src/java/Primera/Ejercicio020.java
@@ -22,7 +22,5 @@ public class Ejercicio020 {
             System.out.println(datos[i]);
 
         }
-
-
     }
 }
---
+++ src/java/Primera/Ejercicio021.java
@@ -0,0 +1,24 @@
+/*
+Pasar una posicion a la izquierda:
+20,30,40,50,10
+ */
+package Primera;
+
+public class Ejercicio021 {
+
+    public static void main(String arg[]) {
+
+        int datos[] = {10, 20, 30, 40, 50};
+        int aux = datos[datos.length - 1];
+
+        for (int i = datos.length - 1; i > 0; i--) {
+            datos[i] = datos[i - 1];
+        }
+
+        datos[0] = aux;
+
+        for (int i = 0; i < datos.length; i++) {
+            System.out.println(datos[i]);
+        }
+    }
+}
