7ec2779da8a3ed1e2b0caa21ad899d5553b342c2
Jm <155852978+javiirmv@users.noreply.github.com>
2024-10-02 09:06:11 +0200

    Ejercicios clase

---
+++ src/java/Primera/Ejercicio030.java
@@ -0,0 +1,35 @@
+/*
+Acumular por filas y por columnas
+            System.out.println(acumFilas[i]);
+            System.out.println(acumColumnas[i]);
+
+ */
+package Primera;
+
+public class Ejercicio030 {
+
+    public static void main(String arg[]) {
+
+        int tabla[][] = {
+            {10, 20, 30, 40},
+            {50, 60, 70, 80,},
+            {90, 100, 110, 120}};
+
+        int acumFilas[] = {0, 0, 0};
+        int acumColumnas[] = {0, 0, 0, 0};
+
+        for (int i = 0; i < tabla.length; i++) {
+            for (int j = 0; j < tabla[i].length; j++) {
+                acumFilas[i] += tabla[i][j];
+                acumColumnas[j] += tabla[i][j];
+            }
+            System.out.println(acumFilas[i]);
+        }
+        
+        System.out.println("Acum columnas");
+
+        for (int i = 0; i <= tabla.length; i++) {
+            System.out.println(acumColumnas[i]);
+        }
+    }
+}
