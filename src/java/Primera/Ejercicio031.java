e50a12881b61f6d65e4db87b2da7863cc884ac22
Jm <155852978+javiirmv@users.noreply.github.com>
2024-10-02 10:09:45 +0200

    Ejercicios clase

--- src/java/Primera/Ejercicio031.java
+++ src/java/Primera/Ejercicio031.java
@@ -4,7 +4,7 @@ Mostrar kilos de cada producto que se han vendido
  */
 package Primera;
 
-public class Ejercicio030 {
+public class Ejercicio031 {
 
     public static void main(String arg[]) {
 
@@ -14,13 +14,22 @@ public class Ejercicio030 {
             {90, 100, 110, 120, 30}};
 
         String productos[] = {"Naranjas", "Manzanas", "Peras"};
+        double precios[] = {1.5, 2, .8};
+
+        double precioTotal = 0;
 
         for (int i = 0; i < tabla.length; i++) {
-            int kilos = 0;
+            double euros = 0;
+
             for (int j = 0; j < tabla[i].length; j++) {
-                kilos += tabla[i][j];
+                euros += tabla[i][j] * precios[i];
             }
-            System.out.printf("Se han vendido %d kilos de %s \n", kilos, productos[i]);
+
+            precioTotal += euros;
+            System.out.printf("Se han vendido %.2f euros de %s \n", euros, productos[i]);
         }
+
+        System.out.printf("En total he ganado %.2f euros\n", precioTotal);
+
     }
 }
