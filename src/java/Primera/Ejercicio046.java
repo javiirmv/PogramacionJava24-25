aae6b9c03b5382989cf65d2dcfd9d78992cb9b07
Jm <155852978+javiirmv@users.noreply.github.com>
2024-10-17 08:00:04 +0200

    Ejercicios clase

---
+++ src/java/Primera/Ejercicio046.java
@@ -0,0 +1,26 @@
+package Primera;
+
+public class Ejercicio046 {
+
+    public static void main(String arg[]) {
+    
+        int vector[] = {2,6,10,13};
+        
+        System.out.println(maximo(vector));
+    
+    }
+
+    public static int maximo(int vector[]){
+        
+        int maximo = vector[0];
+        
+        for (int i = 1; i < vector.length; i++){
+            if (vector[i] > maximo){
+                maximo = vector[i];
+            }
+        }
+        
+        return maximo;
+    }
+    
+}
---
+++ src/java/Primera/Minesweeper.java
@@ -0,0 +1,45 @@
+package Primera;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Minesweeper {
+
+    public static void main(String arg[]) {
+
+        Random rand = new Random();
+
+        boolean finish = false;
+        int lives = 6;
+        int randomNumber = rand.nextInt(101);
+
+        System.out.println("El número aleatorio es:" + randomNumber);
+        
+        while (!finish && lives > 0) {
+
+            System.out.println("Seleccione un número del 1 al 100.");
+            Scanner in = new Scanner(System.in);
+            int num = in.nextInt();
+
+            if (num >= 0 && num <= 100) {
+                lives--;
+                
+                if (randomNumber == num) {
+                    System.out.println("Has ganado!");
+                    finish = true;
+                } else if (randomNumber > num){
+                    System.out.println("El número introducido es menor");
+                } else {
+                    System.out.println("El número introducido es mayor"); 
+                }
+
+            } else {
+                System.out.println("No has introducido un número del 1 al 100, vuelva a intentarlo.");
+            }
+
+            if (!finish){
+                System.out.println("Te quedan " + lives + " intentos");
+            }
+        }
+    }
+}
