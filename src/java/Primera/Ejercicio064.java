cbb69a85ab11dc09e9b5ea27abfebc5e9aff9cd2
Jm <155852978+javiirmv@users.noreply.github.com>
2024-10-30 09:37:51 +0100

    Ejercicios clase

---
+++ src/java/Primera/Ejercicio064.java
@@ -0,0 +1,28 @@
+package Primera;
+
+public class Ejercicio064 {
+
+    public static void main(String arg[]) {
+        char frase[] = {'E', 'n', 'n', 'n', 'n', ' ', 'u', 'n', 'n', 'n', ' ', 'l', 'u', 'g', 'a', 'r', ' ', 'd', 'e', ' ', 'l', 'a', ' ', 'n',  'n', 'n', 'n', 'M', 'a', 'n', 'c', 'h', 'a'};
+        char subcadena[] = {'n', 'n', 'n'};
+        int contador = 0;
+
+        for (int i = 0; i < frase.length - 1; i++) {
+            int subContador = 0;
+
+            for (int x = 0; x < subcadena.length && i < frase.length-subcadena.length-1; x++) {
+                if (frase[i + x] == subcadena[x]) {
+                    subContador++;
+                }
+
+            }
+            if (subContador == subcadena.length) {
+                contador++;
+            }
+
+        }
+
+        System.out.println(contador);
+
+    }
+}
