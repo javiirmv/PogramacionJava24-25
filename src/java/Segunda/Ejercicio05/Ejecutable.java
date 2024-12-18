d90700f3515ffc72d3d240e4cfafdc6da343637e
Jm <155852978+javiirmv@users.noreply.github.com>
2024-12-18 09:34:10 +0100

    Ejercicios Clase

---
+++ src/java/Segunda/Ejercicio05/Ejecutable.java
@@ -0,0 +1,7 @@
+package Segunda.Ejercicio05;
+
+public class Ejecutable {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/java/Segunda/Ejercicio05/OvalApp.java
@@ -0,0 +1,48 @@
+package Segunda.Ejercicio05;
+
+import java.awt.Button;
+import java.awt.Event;
+import java.awt.Frame;
+import java.awt.Panel;
+
+public class OvalApp extends Frame {
+
+    public static void main(String[] args) {
+        OvalApp app = new OvalApp();
+    }
+
+    public OvalApp() {
+        super("Dibujando óvalos");
+
+        this.show();
+        this.pack();
+        this.resize(900, 900);
+
+        Panel panel = new Panel();
+
+        Button boton1 = new Button("Siguiente");
+        panel.add("South", boton1);
+
+        Button boton2 = new Button("Salir");
+        panel.add("South", boton2);
+
+        this.add("South", panel);
+    }
+
+    public boolean handleEvent(Event ev) {
+        if (ev.id == Event.WINDOW_DESTROY) {
+            System.exit(0);
+            return true;
+        } else if (ev.id == Event.ACTION_EVENT) {
+            if (ev.target instanceof Button) {
+                if (ev.arg == "Salir") {
+                    System.exit(0);
+                    return true;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+}
