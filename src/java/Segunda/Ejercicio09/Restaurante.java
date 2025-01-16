98250f97f5952dfbc990a0c4e9f8da445e6d977e
Jm <155852978+javiirmv@users.noreply.github.com>
2025-01-16 10:08:22 +0100

    Ejercicios Clase

--- src/java/Segunda/Ejercicio09/Restaurante.java
+++ src/java/Segunda/Ejercicio09/Restaurante.java
@@ -1,8 +1,13 @@
 package Segunda.Ejercicio09;
 
+import java.awt.Choice;
 import java.awt.Event;
 import java.awt.Frame;
-
+import java.awt.Label;
+import java.awt.List;
+import java.awt.TextField;
+import java.awt.Checkbox;
+import java.awt.Color;
 
 public class Restaurante extends Frame {
 
@@ -10,9 +15,13 @@ public class Restaurante extends Frame {
     String platos[][] = {
         {"Café", "Leche", "Tostadas", "Aire"},
         {"Paella", "Sopa", "Macarrones", "Cachopo"},
-        {"Tortilla", "Sopa", "Arroz 534 delicias", "Kebab"},
-    };
-    
+        {"Tortilla", "Sopa", "Arroz 534 delicias", "Kebab"},};
+
+    Choice chComida;
+    List[] listasPlatos;
+    List presentar;
+    TextField texto;
+
     public static void main(String[] args) {
         Restaurante app = new Restaurante();
     }
@@ -23,13 +32,38 @@ public class Restaurante extends Frame {
         setUp();
 
         this.pack(); // Meter dentro del frame
-        this.setSize(400, 300); // Pone el tamaño del objeto
+        this.setSize(800, 600); // Pone el tamaño del objeto
         this.setVisible(true);
 
     }
 
     public void setUp() {
-       
+
+        this.add("North", new Label("Elige tu comanda", Label.CENTER));
+        this.chComida = new Choice();
+
+        for (int i = 0; i < comidas.length; i++) {
+            chComida.add(comidas[i]);
+        }
+
+        this.add("West", chComida);
+
+        this.listasPlatos = new List[this.comidas.length];
+
+        for (int i = 0; i < this.comidas.length; i++) {
+            listasPlatos[i] = new List(5, true);
+
+            for (int j = 0; j < platos[i].length; j++) {
+                listasPlatos[i].add(platos[i][j]);
+            }
+
+        }
+
+        presentar = listasPlatos[0];
+        this.add("East", presentar);
+        texto = new TextField("", 40);
+
+        this.add("South", texto);
 
     }
 
@@ -38,6 +72,27 @@ public class Restaurante extends Frame {
             System.exit(0);
             return true;
 
+        } else if (ev.id == Event.ACTION_EVENT) {
+            if (ev.target instanceof Choice) {
+                this.remove(presentar);
+
+                presentar = this.listasPlatos[chComida.getSelectedIndex()];
+
+                texto.setText(comidas[chComida.getSelectedIndex()].concat(": "));
+
+                this.add("East", presentar);
+                this.setVisible(true);
+            }
+        } else if (ev.id == Event.LIST_SELECT || ev.id == Event.LIST_DESELECT) {
+            
+            String frase = comidas[chComida.getSelectedIndex()].concat(": ");
+            
+            for (int i = 0; i < presentar.getSelectedItems().length; i++){
+                frase = frase.concat(presentar.getSelectedItems()[i]).concat(" ");
+            }
+            
+            
+            texto.setText(frase);
         }
 
         return false;
