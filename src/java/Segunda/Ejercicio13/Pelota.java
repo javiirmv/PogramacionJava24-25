c046cd1709f84cdcf4d904993fd6680a864e388a
Jm <155852978+javiirmv@users.noreply.github.com>
2025-01-30 08:03:05 +0100

    Ejercicios Clase

--- src/java/Segunda/Ejercicio13/Pelota.java
+++ src/java/Segunda/Ejercicio13/Pelota.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Segunda.Ejercicio13;
 
-/**
- *
- * @author Javi
- */
-public class Pelota {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Pelota extends Rectangle {
+
+    int velX, velY;
+    Color color;
+
+   Color colores[] = {Color.RED, Color.YELLOW, Color.MAGENTA, Color.BLUE, Color.ORANGE};
+    
+    public Pelota() {
+        super((int) (Math.random() * 250), (int) (Math.random() * 250), 0, 0);
+
+        int radio = (int) ((Math.random() * 30) + 20);
+
+        this.height = this.width = radio;
+
+        int velX = (int) ((Math.random() * 11) - 5);
+        int velY = (int) ((Math.random() * 11) - 5);
+
+        Color color = colores[(int) (Math.random() * colores.length)];
+        
+    }   
+
+    
+    public void paint(Graphics g){
+        g.fillOval(x, y, width, height);
+    
+    }
+    
+    public void actualizar(){
+    
+    }
     
 }
