767397576e56af0155293dcf75f1128ee2b7491d
Jm <155852978+javiirmv@users.noreply.github.com>
2025-02-07 13:50:49 +0100

    Ejercicios Clase

---
+++ src/java/Segunda/Ejercicio15/Columna.java
@@ -0,0 +1,32 @@
+package Segunda.Ejercicio15;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Columna {
+
+    Rectangle top;
+    Rectangle bottom;
+
+    public Columna() {
+        int alturaRandom = (int) ((Math.random() * 105) + 20);
+        top = new Rectangle(300 - 30, 0, 30, alturaRandom);
+        bottom = new Rectangle(300 - 30, alturaRandom + 75, 30, 300 - 75 - alturaRandom);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.green);
+        g.fillRect(top.x, top.y, top.width, top.height);
+        g.fillRect(bottom.x, bottom.y, bottom.width, bottom.height);
+
+    }
+
+    public boolean update() {
+        top.x = top.x - 10;
+        bottom.x = bottom.x - 10;
+
+        return true;
+    }
+
+}
--- src/java/Segunda/Ejercicio15/FlappyBird.java
+++ src/java/Segunda/Ejercicio15/FlappyBird.java
@@ -1,10 +1,13 @@
 package Segunda.Ejercicio15;
 
+import Segunda.Ejercicio14.Ladrillo;
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Event;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FlappyBird extends Applet implements Runnable {
 
@@ -12,11 +15,18 @@ public class FlappyBird extends Applet implements Runnable {
     Image imagen;
     Graphics noseve;
     Thread animacion;
+        public static final int FILAS = 5;
+
+    
+    int contColumnas = 10;
+
+    List<Columna> columnas;
 
     public void init() {
         imagen = this.createImage(300, 300);
         noseve = imagen.getGraphics();
         this.setSize(300, 300);
+        columnas = new ArrayList<Columna>();
 
         pajaro = new Pajaro();
 
@@ -33,24 +43,37 @@ public class FlappyBird extends Applet implements Runnable {
         noseve.fillRect(0, 0, 300, 300);
 
         pajaro.paint(noseve);
+        for (Columna columna : columnas) {
+            columna.paint(noseve);
+        }
         g.drawImage(imagen, 0, 0, this);
 
     }
 
-    public boolean keyDown(Event ev, int tecla){
-        if (tecla == Event.UP){
+    public boolean keyDown(Event ev, int tecla) {
+        if (tecla == Event.UP) {
             pajaro.saltar();
         }
         return true;
     }
-    
-    
+
     @Override
     public void run() {
         do {
-            System.out.println("as");
+            for (Columna columna : columnas) {
+                columna.update();
+            }
             pajaro.update();
             repaint();
+
+            if (contColumnas >= 10) {
+                contColumnas = 0;
+                columnas.add(new Columna());
+
+            } else {
+            
+                contColumnas++;
+            }
 
             try {
                 Thread.sleep(50);
