e03c55e000189c236ad7b12bffb0407200f4c56d
palazonfotos <morer.vajav22@zaragoza.salesianos.edu>
2025-04-02 11:13:13 +0200

    Ejercicios Clase
    3

--- src/java/Tercera/Ejercicio06/Ruleta.java
+++ src/java/Tercera/Ejercicio06/Ruleta.java
@@ -8,24 +8,39 @@ import java.awt.Image;
 
 public class Ruleta extends Applet {
 
-    public static final int DIM = 10;
+    public static final int FILAS = 12;
+    public static final int COLUMNAS = 3;
 
     Image imagen;
     Graphics noseve;
 
-    Casilla casillas[][] = new Casilla[DIM][DIM];
-    Casilla prueba;
+    Casilla casillas[][];
 
-    Casilla prueba2;
+    public int rojos[] = {1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36};
 
     public void init() {
-        imagen = this.createImage(400, 500);
+        imagen = this.createImage(400, 900);
         noseve = imagen.getGraphics();
 
-        prueba = new Casilla(50, 50, 50, Color.red);
-        prueba2 = new Casilla(110, 50, 50, Color.BLACK);
+        casillas = new Casilla[COLUMNAS][FILAS];
 
-        this.setSize(400, 500);
+        for (int i = 0; i < COLUMNAS; i++) {
+            for (int j = 0; j < FILAS; j++) {
+                Color color = Color.black;
+                int num = i * FILAS + j + 1;
+                for (int z = 0; z < rojos.length; z++) {
+                    if (num == rojos[z]) {
+                        color = color.red;
+                    }
+                }
+
+                casillas[i][j] = new Casilla(Casilla.DIM * i + 30, Casilla.DIM * j + 30, num, color);
+
+
+            }
+        }
+
+        this.setSize(400, 900);
 
     }
 
@@ -35,10 +50,15 @@ public class Ruleta extends Applet {
 
     public void paint(Graphics g) {
         noseve.setColor(Color.DARK_GRAY);
-        noseve.fillRect(0, 0, 700, 700);
+        noseve.fillRect(0, 0, 400, 900);
         noseve.setColor(Color.GRAY);
-        prueba.paint(noseve);
-        prueba2.paint(noseve);
+
+        for (int i = 0; i < COLUMNAS; i++) {
+            for (int j = 0; j < FILAS; j++) {
+                casillas[i][j].paint(noseve);
+
+            }
+        }
 
         g.drawImage(imagen, 0, 0, this);
 
